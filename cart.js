1f98c814c2325c3d5d928c3aa5f108b9b4c16b6c
cmodtohan <cmodtohan@gmail.com>
2023-04-11 20:05:17 +0800

    Activity committed By: Christ Marlon Odtohan

--- cart.js
+++ cart.js
@@ -1,8 +1,24 @@
-//1. Select the "Add to Cart" button
-//2. Select the cart
-//3. Initialize the cart total
-//4. Add a click event listener to the "Add to Cart" button
-//5. Get the product information
-//6. Create a new cart item
-//7. Add the cart item to the cart
-//8. Update the cart total
+// 1. Select the "Add to Cart" button
+const addToCartBtn = document.querySelector(".add-to-cart");
+
+// 2. Select the cart
+const cart = document.querySelector(".cart ul");
+
+// 3. Initialize the cart total
+let cartTotal = 0;
+
+// 4. Add a click event listener to the "Add to Cart" button
+addToCartBtn.addEventListener("click", () => {
+  // 5. Get the product information
+  const productTitle = document.querySelector(".product h2").textContent;
+  const productPrice = parseFloat(document.querySelector(".product p").textContent.slice(1));
+
+  // 6. Create a new cart item
+  cart.insertAdjacentHTML("beforeend", `<li><span>${productTitle}</span><span>$${productPrice.toFixed(2)}</span></li>`);
+
+  // 7. Add the cart item to the cart
+  cartTotal += productPrice;
+
+  // 8. Update the cart total
+  document.querySelector(".cart p").textContent = `Total: $${cartTotal.toFixed(2)}`;
+});
